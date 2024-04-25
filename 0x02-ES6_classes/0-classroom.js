cd902b693afaf7742871e01c76fc82dc1d5d9de1
sebsibe23 <sebsibe1990@proton.me>
2024-04-25 08:53:05 -0700

    second

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents the class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
