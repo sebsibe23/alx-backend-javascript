4829665de05d1f231153a742f69d86310c8e4a83
sebsibe23 <sebsibe1990@proton.me>
2024-06-18 17:56:07 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,13 @@
-// Display a welcome message to the user
-process.stdout.write(
-  'Welcome to Holberton School, what is your name?\n'
-);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Set up a readable event listener on stdin
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
-  // If a chunk is read, display the user's name
   if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-// Set up an end event listener on stdin
 process.stdin.on('end', () => {
-  // Display a closing message when input ends
-  process.stdout.write(
-    'This important software is now closing\n'
-  );
+  process.stdout.write('This important software is now closing\n');
 });
