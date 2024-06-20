4c105a013705cdf1b22da03eaf1f0e22db52f664
sebsibe23 <sebsibe1990@proton.me>
2024-06-20 11:22:14 -0700

    fix task 4

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,22 +1,7 @@
-/**
- * Function that sends a payment request to an API using Utils for calculation.
- *
- * @param {number} totalAmount - The total amount of the payment.
- * @param {number} totalShipping - The total shipping cost.
- */
 const Utils = require('./utils');
 
-/**
- * Sends a payment request to an API and logs the total cost.
- *
- * @param {number} totalAmount - The total amount of the payment.
- * @param {number} totalShipping - The total shipping cost.
- */
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  // Calculate the total cost by summing totalAmount and totalShipping
   const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-
-  // Log the total cost to the console
   console.log(`The total is: ${totalCost}`);
 };
 
